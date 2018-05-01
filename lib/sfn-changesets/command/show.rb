0189891defc249a9c12f6eb2de1d3c2b2406b118
Michael F. Weinberg <mweinberg@seatgeek.com>
2018-04-30 21:02:12 -0700

    Improve change set display

--- lib/sfn-changesets/command/show.rb
+++ lib/sfn-changesets/command/show.rb
@@ -42,6 +42,72 @@ module Sfn
         return diffs
       end
 
+      def display_change(change, parameters)
+        ui.table(self) do
+          table(border: false) do
+            row(header: true) do
+              column 'Action', width: 8
+              column 'Resource', width: 25
+              column 'Physical Id', width: 62
+              column 'Type', width: 40
+              column 'Replacement?', width: 15
+            end
+            cols = %w(action resource resource_id type replacement)
+            row do
+              cols.each do |attr|
+                column change[attr]
+              end
+            end
+          end
+        end.display
+        return
+      end
+
+      def display_details(change, parameters)
+        ui.table(self) do
+          table(border: false) do
+            row(header: true) do
+              column nil, width: 8
+              column '-' * 127, width: 129
+              column nil, width: 20
+            end
+          end
+          change['reasons'].each do |detail|
+            row do
+              column nil
+              column compose_reason(detail, parameters)
+              column detail['recreation']
+            end
+          end
+        end.display
+        return
+      end
+
+      def display_diff(change, diffs)
+        if diffs[change['resource']][0]
+          ui.table(self) do
+            table(border: false) do
+              row do
+                column 'Diff', width: 8
+                diff = diffs[change['resource']]
+                if diff[0]
+                  case diff[0][0]
+                  when '+'
+                    color = :green
+                  when '~'
+                    color = 'orange'
+                  when '-'
+                    color = :red
+                  end
+                end
+                column ui.color(diffs[change['resource']][0].map { |o| o.is_a?(Hash) ? o.to_json : o }.join(' '), color), width: 130 if diff[0]
+              end
+            end
+          end.display
+        end
+        ui.puts "\n"
+      end
+
       def show_set(stack, set)
         client = cfn_client
         resp = client.describe_change_set(
@@ -62,10 +128,13 @@ module Sfn
           {
             'action' => change.resource_change.action,
             'resource' => change.resource_change.logical_resource_id,
+            'resource_id' => change.resource_change.physical_resource_id,
             'type' => change.resource_change.resource_type,
+            'replacement' => change.resource_change.replacement,
             'reasons' => change.resource_change.details.map do |detail|
               {
                 'attribute' => detail.target.attribute.gsub('Properties', 'Property'),
+                'recreation' => detail.target.requires_recreation,
                 'name' => detail.target.name.to_s,
                 'evaluation' => detail.evaluation,
                 'source' => detail.change_source,
@@ -76,71 +145,16 @@ module Sfn
         end
         diffs = resource_diffs(changes, stack, set)
         changes.sort_by! { |change| change['action'] }
-        cols = %w(action resource reasons)
+        #cols = %w(action resource reasons)
         ui.info "Created: #{resp.creation_time}"
         ui.info "Status: #{resp.status}"
         ui.info "Execution Status: #{resp.execution_status}"
-        ui.info "Changes:"
-        ui.table(self) do
-          table(border: false) do
-            row(header: true) do
-              column 'Action', width: 8
-              column 'Resource', width: width_val = changes.map { |e| e['type'].to_s.length }.max + 2
-              column 'Reason(s)', width: 90
-            end
-            cols.delete('reasons')
-            changes.each do |change|
-              lines = change['reasons'].size - 2
-              row do
-                cols.each do |attr|
-                  column change[attr]
-                end
-                unless change['reasons'].empty?
-                  reason = change['reasons'][0]
-                  column compose_reason(reason, parameters)
-                end
-              end
-              row do
-                column nil
-                column change['type']
-                if change['reasons'].size > 1
-                  column compose_reason(change['reasons'][1], parameters)
-                end
-              end
-              lines.times do |i|
-                i = i + 2
-                row do
-                  cols.each do |attr|
-                    column nil
-                  end
-                  reason = change['reasons'][i]
-                  column compose_reason(reason, parameters)
-                end
-              end
-              if diffs[change['resource']][0]
-                row do
-                  diff = diffs[change['resource']]
-                  if diff[0]
-                    case diff[0][0]
-                    when '+'
-                      color = :green
-                    when '~'
-                      color = 'gold'
-                    when '-'
-                      color = :red
-                    end
-                  end
-                  column nil
-                  column 'Diff'
-                  column ui.color(diffs[change['resource']][0].map { |o| o.is_a?(Hash) ? o.to_json : o }.join(' '), color) if diff[0]
-                end
-              end
-              row do
-                column nil
-              end
-            end
-          end
-        end.display
+        ui.info "Resource Changes:"
+        changes.each do |change|
+          display_change(change, parameters)
+          display_details(change, parameters)
+          display_diff(change, diffs)
+        end
         return
       end
     end
