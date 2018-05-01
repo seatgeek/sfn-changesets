a0349d0d12a6d26f6eed4248466fb628cc211075
Michael F. Weinberg <mweinberg@seatgeek.com>
2018-04-30 21:01:38 -0700

    Poll events on change set apply

--- lib/sfn-changesets/command/apply.rb
+++ lib/sfn-changesets/command/apply.rb
@@ -9,6 +9,20 @@ module Sfn
           stack_name: stack,
           change_set_name: set
         )
+        if config[:poll]
+          poll_stack = provider.stacks.get(stack)
+          poll_stack(stack)
+          if poll_stack.reload.state == :update_complete
+            ui.info "Change Set applied: #{ui.color('SUCCESS', :green)}"
+            namespace.const_get(:Describe).new({:outputs => true}, [stack]).execute!
+          else
+            ui.fatal "Change Set #{ui.color(set, :bold)}: #{ui.color('FAILED', :red, :bold)}"
+            raise 'Stack did not reach a successful update completion state.'
+          end
+        else
+          ui.warn 'Stack state polling has been disabled.'
+          ui.info "Initialized apply of Change Set #{ui.color(set, :green)} to #{ui.color(stack, :green)}"
+        end
         return
       end
     end
