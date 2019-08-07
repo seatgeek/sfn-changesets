b2b9602b47882aa6333c28f10e1910bda2d339f2
Michael F. Weinberg <mweinberg@seatgeek.com>
2019-08-07 16:06:26 -0700

    Add support for tags from stack file. Set proper defaults

--- lib/sfn-changesets/config/parameter_file.rb
+++ lib/sfn-changesets/config/parameter_file.rb
@@ -7,9 +7,11 @@ def load_stack_file(stack_name)
     Smash.new
   else
     stack_file = Bogo::Config.new(paths.first).data
-    config[:compile_parameters] = stack_file[:compile_parameters]
-    config[:parameters] = stack_file[:parameters]
+    config[:compile_parameters] = stack_file[:compile_parameters] || Smash.new
+    config[:parameters] = stack_file[:parameters] || Smash.new
     config[:file] = stack_file[:template]
-    config[:apply_stacks] = stack_file[:apply_stacks]
+    config[:apply_stacks] = stack_file[:apply_stacks] || []
+    config[:apply_mapping] ||= Smash.new
+    config[:options][:tags] ||= Smash.new
   end
 end
